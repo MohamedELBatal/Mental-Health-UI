3f91fd8736a05d51e1ae4fbeb6599d1bd712d82b
Admin <beshovetch@gmail.com>
2024-08-16 15:25:26 +0400

    some_edit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,35 +1,42 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mental_health_app/workout_screen.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:badges/badges.dart' as badges;
 
-class HomeScreen extends StatelessWidget {
+import 'news_screen.dart';
+
+class HomeScreen extends StatefulWidget {
   static const String routeName = "HomeScreen";
 
   HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: Image.asset("assets/images/d1/logo1.png"),
-          title: const Text("Moody"),
-          actions:  [
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: badges.Badge(
-                position: badges.BadgePosition.custom(top: 0,end: 2),
-                child: const Icon(Icons.notifications_none_outlined),
-              ),
+      appBar: AppBar(
+        leading: Image.asset("assets/images/d1/logo1.png"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: badges.Badge(
+              position: badges.BadgePosition.custom(top: 0, end: 2),
+              child: const Icon(Icons.notifications_none_outlined),
             ),
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             const Text(
               "Hello, Sara Rose",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
@@ -113,7 +120,10 @@ class HomeScreen extends StatelessWidget {
                 ),
                 Text(
                   "See more >",
-                  style: TextStyle(color: Colors.green,fontWeight:FontWeight.w600,fontSize: 14),
+                  style: TextStyle(
+                      color: Colors.green,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14),
                 ),
               ],
             ),
@@ -137,9 +147,9 @@ class HomeScreen extends StatelessWidget {
                 effect: const WormEffect(dotHeight: 10, dotWidth: 10),
               ),
             ),
-                const SizedBox(
-                  height: 35,
-                ),
+            const SizedBox(
+              height: 35,
+            ),
             const Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -149,36 +159,59 @@ class HomeScreen extends StatelessWidget {
                 ),
                 Text(
                   "See more >",
-                  style: TextStyle(color: Colors.green,fontWeight:FontWeight.w600,fontSize: 14),
+                  style: TextStyle(
+                      color: Colors.green,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14),
                 ),
               ],
             ),
-                const SizedBox(
+            const SizedBox(
+              height: 15,
+            ),
+            Column(
+              children: [
+                Center(
+                  child: Image.asset("assets/images/d1/exercise1.png"),
+                ),
+                SizedBox(
                   height: 15,
                 ),
-                Column(
-                  children: [
-                   Center(child: Image.asset("assets/images/d1/exercise1.png"),),
-                   SizedBox(height: 15,),
-                   Center(child: Image.asset("assets/images/d1/exercise2.png")),
-                  ],
-                ),
-                const Spacer(),
-                BottomNavigationBar(
-                  elevation: 0,
-                  iconSize: 30,
-                  unselectedItemColor: Colors.grey[500],
-                    selectedItemColor: Colors.green,
-                    showUnselectedLabels: false,
-                    showSelectedLabels: false,
-                    items:  const [
-                  BottomNavigationBarItem(icon: Icon(Icons.home),label: ""),
-                  BottomNavigationBarItem(icon: Icon(Icons.grid_view_outlined),label: ""),
-                  BottomNavigationBarItem(icon: Icon(Icons.calendar_month_outlined),label: ""),
-                  BottomNavigationBarItem(icon: Icon(Icons.person_2_outlined),label: ""),
-                ])
-          ]
-              ),
-        ));
+                Center(child: Image.asset("assets/images/d1/exercise2.png")),
+              ],
+            ),
+            const Spacer(),
+            BottomNavigationBar(
+              currentIndex: index,
+              onTap: (int tappedIndex) {
+                setState(() {
+                  index = tappedIndex;
+                });
+
+                // Handle navigation based on tapped index
+                if (index == 1) {
+                  Navigator.pushNamed(context, NewsScreen.routeName);
+                } else if (index == 2) {
+                  Navigator.pushNamed(context, WorkoutScreen.routeName);
+                }
+              },
+              elevation: 0,
+              iconSize: 30,
+              unselectedItemColor: Colors.grey[500],
+              selectedItemColor: Colors.green,
+              showUnselectedLabels: false,
+              showSelectedLabels: false,
+              items: const [
+                BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+                BottomNavigationBarItem(icon: Icon(Icons.grid_view_outlined), label: ""),
+                BottomNavigationBarItem(icon: Icon(Icons.calendar_month_outlined), label: ""),
+                BottomNavigationBarItem(icon: Icon(Icons.person_2_outlined), label: ""),
+              ],
+            )
+
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/news_screen.dart
+++ lib/news_screen.dart
@@ -12,205 +12,209 @@ class NewsScreen extends StatelessWidget {
     return DefaultTabController(
         length: 4,
         child: Scaffold(
+          resizeToAvoidBottomInset: true,
+          appBar: AppBar(toolbarHeight: 30,),
           body: SafeArea(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          "assets/images/d3/flower.png",
-                          fit: BoxFit.fill,
+            child: SingleChildScrollView(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            "assets/images/d3/flower.png",
+                            fit: BoxFit.fill,
+                          ),
+                          Text(
+                            "ALiceCare",
+                            style: GoogleFonts.milonga(
+                                fontWeight: FontWeight.w400, fontSize: 24),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SearchBar(
+                      padding: MaterialStatePropertyAll(
+                        EdgeInsets.only(top: 10, bottom: 10, right: 14, left: 14),
+                      ),
+                      leading: Icon(Icons.search),
+                      hintText: "Articles, Video, Audio and More,.....",
+                    ),
+                    const TabBar(
+                      tabs: [
+                        Tab(
+                          child: Text(
+                            "Discover",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w500),
+                          ),
                         ),
-                        Text(
-                          "ALiceCare",
-                          style: GoogleFonts.milonga(
-                              fontWeight: FontWeight.w400, fontSize: 24),
+                        Tab(
+                          child: Text(
+                            "News",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Tab(
+                          child: Text(
+                            "Most Viewed",
+                            style: TextStyle(
+                                fontSize: 11, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Tab(
+                          child: Text(
+                            "Saved",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w500),
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                  const SearchBar(
-                    padding: MaterialStatePropertyAll(
-                      EdgeInsets.only(top: 10, bottom: 10, right: 14, left: 14),
+                    const SizedBox(
+                      height: 5,
                     ),
-                    leading: Icon(Icons.search),
-                    hintText: "Articles, Video, Audio and More,.....",
-                  ),
-                  const TabBar(
-                    tabs: [
-                      Tab(
-                        child: Text(
-                          "Discover",
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w500),
-                        ),
-                      ),
-                      Tab(
-                        child: Text(
-                          "News",
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w500),
-                        ),
-                      ),
-                      Tab(
-                        child: Text(
-                          "Most Viewed",
-                          style: TextStyle(
-                              fontSize: 11, fontWeight: FontWeight.w500),
-                        ),
-                      ),
-                      Tab(
-                        child: Text(
-                          "Saved",
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w500),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(18.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Hot Topics",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Text(
-                          "See more >",
-                          style: TextStyle(
-                              color: Color(0xFF6941C6),
+                    const Padding(
+                      padding: EdgeInsets.all(18.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Hot Topics",
+                            style: TextStyle(
+                              fontSize: 18,
                               fontWeight: FontWeight.w600,
-                              fontSize: 14),
-                        ),
-                      ],
-                    ),
-                  ),
-                  ImageSlideshow(
-                    width: double.infinity,
-                    height: 150,
-                    initialPage: 0,
-                    indicatorColor: Colors.blue,
-                    indicatorBackgroundColor: Colors.grey,
-                    onPageChanged: (value) {
-                      print('Page changed: $value');
-                    },
-                    autoPlayInterval: 3000,
-                    isLoop: true,
-                    children: [
-                      Image.asset("assets/images/d3/self_care.png"),
-                      Image.asset("assets/images/d3/cycle.png"),
-                    ],
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Get Tips",
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.w600),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    height: 200,
-                    width: 350,
-                    color: Colors.grey[100],
-                    child: Row(
-                      children: [
-                        Image.asset("assets/images/d3/doctor.png"),
-                        const SizedBox(
-                          width: 20,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(18.0),
-                          child: Column(
-                            children: [
-                              const Text("Connect with doctors & "),
-                              const Text("get suggestions"),
-                              const Text("Connect now and get"),
-                              const Text("expert insights "),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              ElevatedButton(
-                                onPressed: () {},
-                                style: const ButtonStyle(
-                                  backgroundColor:
-                                      MaterialStatePropertyAll(Colors.blue),
-                                ),
-                                child: const Text(
-                                  "View Detail",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 14),
-                                ),
-                              ),
-                            ],
+                            ),
                           ),
-                        ),
+                          Text(
+                            "See more >",
+                            style: TextStyle(
+                                color: Color(0xFF6941C6),
+                                fontWeight: FontWeight.w600,
+                                fontSize: 14),
+                          ),
+                        ],
+                      ),
+                    ),
+                    ImageSlideshow(
+                      width: double.infinity,
+                      height: 150,
+                      initialPage: 0,
+                      indicatorColor: Colors.blue,
+                      indicatorBackgroundColor: Colors.grey,
+                      onPageChanged: (value) {
+                        print('Page changed: $value');
+                      },
+                      autoPlayInterval: 3000,
+                      isLoop: true,
+                      children: [
+                        Image.asset("assets/images/d3/self_care.png"),
+                        Image.asset("assets/images/d3/cycle.png"),
                       ],
                     ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Cycle Phases and Period",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
+                    const Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Get Tips",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w600),
                           ),
-                        ),
-                        Text(
-                          "See more >",
-                          style: TextStyle(
-                              color: Color(0xFF5925DC),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 200,
+                      width: 350,
+                      color: Colors.grey[100],
+                      child: Row(
+                        children: [
+                          Image.asset("assets/images/d3/doctor.png"),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(18.0),
+                            child: Column(
+                              children: [
+                                const Text("Connect with doctors & "),
+                                const Text("get suggestions"),
+                                const Text("Connect now and get"),
+                                const Text("expert insights "),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  style: const ButtonStyle(
+                                    backgroundColor:
+                                        MaterialStatePropertyAll(Colors.blue),
+                                  ),
+                                  child: const Text(
+                                    "View Detail",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Cycle Phases and Period",
+                            style: TextStyle(
+                              fontSize: 18,
                               fontWeight: FontWeight.w600,
-                              fontSize: 14),
-                        ),
-                      ],
+                            ),
+                          ),
+                          Text(
+                            "See more >",
+                            style: TextStyle(
+                                color: Color(0xFF5925DC),
+                                fontWeight: FontWeight.w600,
+                                fontSize: 14),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  BottomNavigationBar(
-                      elevation: 0,
-                      currentIndex: 1,
-                      iconSize: 35,
-                      unselectedItemColor: Colors.black,
-                      selectedItemColor: Color(0xFF6941C6),
-                      showUnselectedLabels: false,
-                      showSelectedLabels: false,
-                      items: const [
-                        BottomNavigationBarItem(
-                            icon: ImageIcon(
-                                AssetImage("assets/images/d3/ic_calendar.png")),
-                            label: ""),
-                        BottomNavigationBarItem(
-                            icon: ImageIcon(
-                                AssetImage("assets/images/d3/ic_insights.png")), label: ""),
-                        BottomNavigationBarItem(
-                            icon: ImageIcon(
-                                AssetImage("assets/images/d3/ic_chat.png")),
-                            label: ""),
-                      ])
-                ]),
+                    BottomNavigationBar(
+                        elevation: 0,
+                        currentIndex: 1,
+                        iconSize: 35,
+                        unselectedItemColor: Colors.black,
+                        selectedItemColor: Color(0xFF6941C6),
+                        showUnselectedLabels: false,
+                        showSelectedLabels: false,
+                        items: const [
+                          BottomNavigationBarItem(
+                              icon: ImageIcon(
+                                  AssetImage("assets/images/d3/ic_calendar.png")),
+                              label: ""),
+                          BottomNavigationBarItem(
+                              icon: ImageIcon(
+                                  AssetImage("assets/images/d3/ic_insights.png")), label: ""),
+                          BottomNavigationBarItem(
+                              icon: ImageIcon(
+                                  AssetImage("assets/images/d3/ic_chat.png")),
+                              label: ""),
+                        ])
+                  ]),
+            ),
           ),
         ));
   }
